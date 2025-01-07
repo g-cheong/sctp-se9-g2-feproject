9385d321937dbae42fea535f6d193cb4b1006457
Gabriel Cheong <gabriel.cheong@screeningeagle.com>
2025-01-07 23:08:33 +0800

    Refactored CartPage

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -9,37 +9,31 @@ use !user.isLoggedIn return <CartEmpty/>
 If user is login display items in CartCard component from user.cart useContext.
 */
 
-import { useContext } from "react";
-import UserContext from "../../context/UserContext";
-import { CartEmptyPage } from "./CartEmptyPage";
 import { CartCard } from "../../components/CartCard/CartCard";
+import { CartNotLoggedInPage } from "./CartNotLoggedInPage";
+import { CartEmptyPage } from "./CartEmptyPage";
+import { useContext } from "react";
 
-const exampleCart = {
-  id: 1,
-  title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-  price: 109.95,
-  description:
-    "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-  category: "men's clothing",
-  image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-  rating: { rate: 3.9, count: 120 },
-  quantity: 5,
-  total: 50,
-};
+import UserContext from "../../context/UserContext";
 
 function CartPage() {
   const userCtx = useContext(UserContext); 
 
+  if(!userCtx.isLoggedIn) {
+    return <CartNotLoggedInPage/>;
+  } 
+
+  if(userCtx.cart === undefined || userCtx.cart.length === 0) {
+    return <CartEmptyPage/>;
+  }
+
   return (
     <div>
-      {userCtx.isLoggedIn 
-        ? <div>
-          {userCtx.cart.map((product) => {
-            return <CartCard key={product.id} product={product}></CartCard>;
-          })}
-          </div>
-        : <CartEmptyPage/>
-      }
+          { 
+            userCtx.cart.map((product) => {
+              return <CartCard key={product.id} product={product}></CartCard>;
+            })
+          }
     </div>
   );
 }
