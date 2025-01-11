d0622b0413a2480db097020f0d27b9ad0d6eacee
Gabriel C <gabriel.cjj@hotmail.com>
2025-01-11 13:18:22 +0800

    Styling for empty/loggedOut cart pages

--- src/pages/CartPage/CartEmptyPage.js
+++ src/pages/CartPage/CartEmptyPage.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import styles from "./CartEmptyPage.module.css"
+import styles from "./CartEmptyLoggedOutPage.module.css"
 
 export const CartEmptyPage = () => {
     return (
-        <div> 
+        <div className={styles.cartEmptyLoggedOut}> 
             <p>
                 Your cart appears to be empty...
                 <Link to={"/"}> Shop now! </Link>
